8e74d922e3d3f02cad18be3a36b0d30cb579dc94
jekipal <11202315198@mhs.dinus.ac.id>
2025-03-03 17:25:26 +0700

    add file

---
+++ Tugas1/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // data pribadi
+        String nim = "A11.2023.15198";
+        String nama = "Naufal Zaki Zain";
+        String alamat = "Wangandowo ";
+        String kota = "Pekalongan";
+        String kodePos = "51156";
+        String telp = "087785638203";
+        String hp = "087785638203";
+        String email = "faizzidan245@gmail.com";
+        
+        // menampilkan data pribadi
+        System.out.println("=== Data Pribadi ===");
+        System.out.println("NIM       : " + nim);
+        System.out.println("Nama      : " + nama);
+        System.out.println("Alamat    : " + alamat);
+        System.out.println("Kota      : " + kota);
+        System.out.println("Kode Pos  : " + kodePos);
+        System.out.println("Telp.     : " + telp);
+        System.out.println("HP        : " + hp);
+        System.out.println("Email     : " + email);
+    }
+}
